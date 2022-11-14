212beb99e4cce8436511c1379996be97ecb8d92d
hey-now <rubinand22@gmail.com>
2022-11-14 13:33:19 -0800

    working home, about, and index

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
+instruments = [
+    {'type': 'Guitar', 'brand': 'Schecter', 'model': 'Diamond Series C-1', 'color': 'Sunburst', 'manufacture_date': '2002'},
+    {'type': 'Guitar', 'brand': 'Martin', 'model': 'Road Series SC-10E', 'color': 'Satin Spruce', 'manufacture_date': '2022'},
+]
+
 # Create your views here.
+def home(request):
+    return render(request, 'home.html')
+def about(request):
+    return render(request, 'about.html')
+def instruments_index(request):
+    return render(request, 'instruments/index.html', { 'instruments': instruments })
