7fd2de693747cb4ca6a72de71983e032e6d9b42d
hey-now <rubinand22@gmail.com>
2022-11-15 10:58:43 -0800

    adding model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Instrument(models.Model):
+    nickname = models.CharField(max_length=100)
+    type = models.CharField(max_length=100)
+    brand = models.CharField(max_length=100)
+    model = models.CharField(max_length=100)
+    color = models.CharField(max_length=100)
+    manf_date = models.IntegerField()
--- main_app/views.py
+++ main_app/views.py
@@ -1,9 +1,5 @@
 from django.shortcuts import render
-
-instruments = [
-    {'imgURL': 'https://imgur.com/hL0AvIC.jpeg', 'nickname': 'Ole Faithful', 'type': 'Guitar', 'brand': 'Schecter', 'model': 'Diamond Series C-1', 'color': 'Flame', 'manufactured_date': '2002'},
-    {'imgURL': 'https://imgur.com/ksNv6KU.jpeg', 'nickname': 'Showstopper', 'type': 'Guitar', 'brand': 'Martin', 'model': 'Road Series SC-10E', 'color': 'Satin Spruce', 'manufactured_date': '2022'},
-]
+from .models import Instrument
 
 # Create your views here.
 def home(request):
@@ -11,4 +7,5 @@ def home(request):
 def about(request):
     return render(request, 'about.html')
 def instruments_index(request):
+    instruments = Instrument.objects.all()
     return render(request, 'instruments/index.html', { 'instruments': instruments })
